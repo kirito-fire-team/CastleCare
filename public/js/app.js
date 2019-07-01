dad6fbcca9d081e39cba616a914c3cee0d583819
Natsu <kirito.flamedragon@gmail.com>
2019-07-01 03:50:58 -0500

    Created the Mobile Javascript button

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+/*-------------------------
+MAIN MENU BARS
+---------------------------*/
+
+/* Open when someone clicks the span */
+function openNav() {
+  document.getElementById('myNav').style.height = "100%";
+}
+
+
+
+/* Close when someone clicks the X symbol */
+function closeNav() {
+  document.getElementById('myNav').style.height = "0%";
+}
